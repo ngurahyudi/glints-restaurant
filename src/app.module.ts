99c2a886676e36e6fb68f106ef528f764d4df9f3
Yudi Pratama, I Gnb <ibagus.yudi@gmail.com>
2022-05-30 13:15:58 +0800

    feat: import required modules, add global pipes and interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import appConfig from './configs/app.config';
+import databaseConfig from './configs/database.config';
+
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { RestaurantMenuModule } from './restaurant-menu/restaurant-menu.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    RestaurantModule,
+    RestaurantMenuModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix(configService.get('app.apiPrefix'), {
+    exclude: ['/'],
+  });
+
+  // use validation pipe (DTO validation)
+  app.useGlobalPipes(
+    // use this to get validation error in details
+    // DEV MODE
+    new ValidationPipe({
+      transform: true,
+    }),
+
+    // use this to override error message to UnprocessableEntityException
+    // PRODUCTION MODE
+    // new ValidationPipe({
+    //   transform: true,
+    //   disableErrorMessages: false,
+    //   exceptionFactory: (errors: ValidationError[]) =>
+    //     new UnprocessableEntityException(),
+    // }),
+  );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const APP_PORT = configService.get('app.port') || 3000;
+  await app.listen(APP_PORT, () => {
+    console.log(`application is running on port ${APP_PORT}`);
+  });
 }
 bootstrap();
