72d6f8408b586163f0165a51a45f1490b01319a6
Yudi Pratama, I Gnb <ibagus.yudi@gmail.com>
2022-08-07 20:33:45 +0800

    refactor: add entity into parameters

--- src/common/helpers/select-query-builder.ts
+++ src/common/helpers/select-query-builder.ts
@@ -5,6 +5,7 @@ import { OperatorEnum } from '../enums';
  * It takes a query builder, an operator, a filter, and a value, and returns a query builder with the
  * filter applied
  * @param queryBuilder - The query builder object that we are using to build our query.
+ * @param {string} entity - The entity of the query.
  * @param {string} operator - The operator to use for the query.
  * @param {string} filter - The name of the column to filter on.
  * @param {any} value - The value of the filter.
@@ -12,6 +13,7 @@ import { OperatorEnum } from '../enums';
  */
 export const selectQuery = <T>(
   queryBuilder: SelectQueryBuilder<T>,
+  entity: string,
   operator: OperatorEnum,
   filter: string,
   value: any,
@@ -19,22 +21,22 @@ export const selectQuery = <T>(
   let qry: SelectQueryBuilder<T>;
   switch (operator) {
     case OperatorEnum.CONTAINS:
-      qry = queryBuilder.andWhere(`restaurant.${filter} LIKE :filter`, {
+      qry = queryBuilder.andWhere(`${entity}.${filter} LIKE :filter`, {
         filter: '%' + value + '%',
       });
       break;
     case OperatorEnum.EQUALS:
-      qry = queryBuilder.andWhere(`restaurant.${filter} = :filter`, {
+      qry = queryBuilder.andWhere(`${entity}.${filter} = :filter`, {
         filter: value,
       });
       break;
     case OperatorEnum.STARTS_WITH:
-      qry = queryBuilder.andWhere(`restaurant.${filter} LIKE :filter`, {
+      qry = queryBuilder.andWhere(`${entity}.${filter} LIKE :filter`, {
         filter: value + '%',
       });
       break;
     default:
-      qry = queryBuilder.andWhere(`restaurant.${filter} LIKE :filter`, {
+      qry = queryBuilder.andWhere(`${entity}.${filter} LIKE :filter`, {
         filter: '%' + value,
       });
   }
